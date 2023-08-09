27d583701920548ea5079cb78e328107e2ac1fe0
Sam Rose <SR765@canterbury.ac.uk>
2023-08-09 12:25:05 +0100

    Added input requirements and fixed put request error handling

--- path-of-exile-builds-API.py
+++ path-of-exile-builds-API.py
@@ -5,20 +5,21 @@ app = Flask(__name__)
 api = Api(app)
 
 build_put_args = reqparse.RequestParser()
-build_put_args.add_argument("Name", type=str, help="Name of the build")
-build_put_args.add_argument("Budget", type=str, help="I.E League-Starter, MidRange, HighEnd")
-build_put_args.add_argument("Pastebin", type=str, help="pastebin link for the builds POB details")
-build_put_args.add_argument("Description", type=str, help="Short description of the build, its strengths and weaknesses")
+build_put_args.add_argument("Name", type=str, help="Name of the build is required", required=True)
+build_put_args.add_argument("Budget", type=str, help="I.E League-Starter, MidRange, HighEnd is required", required=True)
+build_put_args.add_argument("Pastebin", type=str, help="pastebin link for the builds POB details is required", required=True)
+build_put_args.add_argument("Description", type=str, help="Short description of the build, its strengths and weaknesses is required", required=True)
 
 #Builds need a: Name, Budget, Pastebin, Description
 builds = {}
 
 class Builds(Resource):
     def get(self, build):
-        return build[builds]
+        return builds[build]
     def put(self, build):
         args = build_put_args.parse_args()
-        return {build: args}
+        builds[build] = args
+        return builds[build], 201
     
 api.add_resource(Builds, "/builds/<string:build>")
 
--- test.py
+++ test.py
@@ -4,3 +4,6 @@ BASE = "http://127.0.0.1:5000/"
 
 response = requests.put(BASE + "builds/1", {"Name": "SST Champion", "Budget": "HighEnd", "Pastebin": "pastebin.URL", "Description": "Fast and tanky"})
 print(response.json())
+input()
+response = requests.get(BASE + "builds/1")
+print(response.json())
