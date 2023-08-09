326a9706fab45f5deb979f59ca491cc325088213
Sam Rose <SR765@canterbury.ac.uk>
2023-08-09 11:23:33 +0100

    Added a more in depth output with subsection capabilities and multi request types

--- path-of-exile-builds-API.py
+++ path-of-exile-builds-API.py
@@ -4,11 +4,14 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+budgets = {"League-Starter": {"RF Inquisitor": "pastebin.URL", "Boneshatter Juggernaut": "pastebin.URL"},
+           "MidRange": {"Impending Doom Pathfinder": "pastebin.URL"}}
+
 class BuildTypes(Resource):
-    def get(self):
-        return {"Type": "Tanky", "Type": "Fast", "Type": "Hybrid"}
+    def get(self, budget):
+        return budgets[budget]
     
-api.add_resource(BuildTypes, "/buildtypes")
+api.add_resource(BuildTypes, "/buildtypes/<string:budget>")
 
 if __name__ == "__main__":
     app.run(debug=True) 
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "buildtypes")
+response = requests.get(BASE + "buildtypes/League-Starter")
 print(response.json())
