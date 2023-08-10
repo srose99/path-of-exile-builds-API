80c39445eb0930847e1edc4084278811322dcc50
Sam Rose <SR765@canterbury.ac.uk>
2023-08-10 12:23:44 +0100

    Large push, added data persistance with sqlalchemy, some errors came with this and put requests though which need handling

--- path-of-exile-builds-API.py
+++ path-of-exile-builds-API.py
@@ -1,37 +1,80 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+from flask_cli import FlaskCLI
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+FlaskCLI(app)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class BuildModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    budget = db.Column(db.String(100), nullable=False)
+    pastebin = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"Build(name = {name}, budget = {budget}, pastebin = {pastebin}, description = {description})"
 
 build_put_args = reqparse.RequestParser()
-build_put_args.add_argument("Name", type=str, help="Name of the build is required", required=True)
-build_put_args.add_argument("Budget", type=str, help="I.E League-Starter, MidRange, HighEnd is required", required=True)
-build_put_args.add_argument("Pastebin", type=str, help="pastebin link for the builds POB details is required", required=True)
-build_put_args.add_argument("Description", type=str, help="Short description of the build, its strengths and weaknesses is required", required=True)
+build_put_args.add_argument("name", type=str, help="Name of the build is required", required=True)
+build_put_args.add_argument("budget", type=str, help="I.E League-Starter, MidRange, HighEnd is required", required=True)
+build_put_args.add_argument("pastebin", type=str, help="pastebin link for the builds POB details is required", required=True)
+build_put_args.add_argument("description", type=str, help="Short description of the build, its strengths and weaknesses is required", required=True)
 
-#Builds need a: Name, Budget, Pastebin, Description
-builds = {}
-
-def abort_if_invalid(build):
-    if build not in builds:
-        abort(404, message="Couldnt find build")
-
-def abort_if_already_exists(build):
-    if build in builds:
-        abort(409, message="Build already exists")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'budget': fields.String,
+    'pastebin': fields.String,
+    'description': fields.String
+}
 
 class Builds(Resource):
+    @marshal_with(resource_fields)
     def get(self, build):
-        abort_if_invalid(build)
-        return builds[build]
+        result = BuildModel.query.filter_by(id=build).first()
+        if not result:
+            abort(404, message="Couldnt be found")
+        return result
     
+    @marshal_with(resource_fields)
     def put(self, build):
-        abort_if_already_exists(build)
         args = build_put_args.parse_args()
-        builds[build] = args
-        return builds[build], 201
+        result = BuildModel.query.filter_by(id=build).first()
+        if result:
+            abort(409, message="build id was taken already")
+        build_data = BuildModel(id=build, name=args['name'], budget=args['budget'], pastebin=args['pastebin'], description=args['description'])
+        db.session.add(build_data)
+        db.session.commit()
+        return build_data, 201
     
+    @marshal_with(resource_fields)
+    def patch(self,build):
+        args = build_put_args.parse_args()
+        result = BuildModel.query.filter_by(id=build).first()
+        if not result:
+            abort(404, message="Build does not exist, cannot update")
+
+        if args['name']:
+            result.name = args["name"]
+        if args['budget']:
+            result.budget = args["budget"]
+        if args['pastebin']:
+            result.pastebin = args["pastebin"]
+        if args['description']:
+            result.description = args["description"]
+        
+        db.session.commit()
+
+        return result
+    
+    @marshal_with(resource_fields)
     def delete(self, build):
         abort_if_invalid(build)
         del builds[build]
--- test.py
+++ test.py
@@ -2,8 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "builds/1", {"Name": "SST Champion", "Budget": "HighEnd", "Pastebin": "pastebin.URL", "Description": "Fast and tanky"})
-print(response.json())
+data = [{"name": "SST Champion", "budget": "HighEnd", "pastebin": "pastebin.URL", "description": "Fast and Tanky"},
+        {"name": "RF Inquisitor", "budget": "League-Starter", "pastebin": "pastebin.URl", "description": "Tanky"}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "builds/" + str(i), json=data[i])
+    if response.status_code == 200:
+        print("Build", i, "created successfully")
+    else:
+        print("Error creating Build", i)
+        print(response.status_code)
+
 input()
 response = requests.get(BASE + "builds/1")
-print(response.json())
+if response.status_code == 200:
+    print(response.json())
+else:
+    print("Error retrieving Build 1")
