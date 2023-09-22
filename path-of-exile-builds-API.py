a70f446084502b4da1622e9703e3c5195f4b3779
Sam Rose <SR765@canterbury.ac.uk>
2023-09-22 11:38:25 +0100

    Refactored code to better accomodate containerisation and deployment

--- path-of-exile-builds-API.py
+++ path-of-exile-builds-API.py
@@ -1,83 +1,59 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
+import flask
+from flask import request, jsonify
 
-app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-class BuildModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    budget = db.Column(db.String(100), nullable=False)
-    pastebin = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f"Build(name = {name}, budget = {budget}, pastebin = {pastebin}, description = {description})"
-    
+Builds = [
+    {
+        "id": 1,
+        "name": "SST Champion", 
+        "budget": "HighEnd", 
+        "pastebin": "pastebin.URL", 
+        "description": "Fast and Tanky"
+    },
+    {   "id": 2, 
+        "name": "RF Inquisitor", 
+        "budget": "League-Starter", 
+        "pastebin": "pastebin.URl", 
+        "description": "Tanky"
+    }
+]
 
-build_put_args = reqparse.RequestParser()
-build_put_args.add_argument("name", type=str, help="Name of the build is required", required=True)
-build_put_args.add_argument("budget", type=str, help="I.E League-Starter, MidRange, HighEnd is required", required=True)
-build_put_args.add_argument("pastebin", type=str, help="pastebin link for the builds POB details is required", required=True)
-build_put_args.add_argument("description", type=str, help="Short description of the build, its strengths and weaknesses is required", required=True)
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>PoE Builds API</h1>
+                <p>A flask api implementation for build info.</p>'''
 
-resource_fields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'budget': fields.String,
-    'pastebin': fields.String,
-    'description': fields.String
-}
+@app.route('/builds/all', methods=['GET'])
+def builds_all():
+    return jsonify(Builds)
 
-class Builds(Resource):
-    @marshal_with(resource_fields)
-    def get(self, build):
-        result = BuildModel.query.filter_by(id=build).first()
-        if not result:
-            abort(404, message="Couldnt be found")
-        return result
-    
-    @marshal_with(resource_fields)
-    def put(self, build):
-        args = build_put_args.parse_args()
-        result = BuildModel.query.filter_by(id=build).first()
-        if result:
-            abort(409, message="build id was taken already")
-        build_data = BuildModel(id=build, name=args['name'], budget=args['budget'], pastebin=args['pastebin'], description=args['description'])
-        db.session.add(build_data)
-        db.session.commit()
-        return build_data, 201
-    
-    @marshal_with(resource_fields)
-    def patch(self,build):
-        args = build_put_args.parse_args()
-        result = BuildModel.query.filter_by(id=build).first()
-        if not result:
-            abort(404, message="Build does not exist, cannot update")
+@app.route('/builds', methods=['GET'])
+def builds_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an ID."
+    results = []
+    for build in Builds:
+        if build['id'] == id:
+            results.append(build)
+    return jsonify(results)
 
-        if args['name']:
-            result.name = args["name"]
-        if args['budget']:
-            result.budget = args["budget"]
-        if args['pastebin']:
-            result.pastebin = args["pastebin"]
-        if args['description']:
-            result.description = args["description"]
-        
-        db.session.commit()
+@app.route('/builds', methods=['POST'])
+def builds_insert():
+    build = request.get_json()
+    Builds.append(build)
+    return "Success: build added"
 
-        return result
-    
-    @marshal_with(resource_fields)
-    def delete(self, build):
-        abort_if_invalid(build)
-        del builds[build]
-        return "", 204
-    
-api.add_resource(Builds, "/builds/<string:build>")
+@app.route('/builds/<id>', methods=['DELETE'])
+def builds_delete():
+    for build in Builds:
+        if build['id'] == int(id):
+            Builds.remove(build)
+    return "Success: Build Removed"
 
-if __name__ == "__main__":
-    app.run(debug=True) 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
